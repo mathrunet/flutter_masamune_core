47ee8d5664c6e95f54a6a1d2492e388173f2ef3c
mathru <support@mathru.net>
2020-06-21 14:43:24 +0900

    Pathの不具合を修正

--- lib/path/component/pathlistener.dart
+++ lib/path/component/pathlistener.dart
@@ -65,7 +65,7 @@ class PathListener {
 
   static void _updateNotify(IPath path) {
     Log.ast(
-        "Enqueue to invoking action: %s (%s)", [path.path, path.toString()]);
+        "Enqueue to invoking action: %s", [path.path]);
     _updateStack.add(path);
   }
 
