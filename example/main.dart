94c27e1d7ea3dd3a540c3a6d4b1971a25b31a5e1
mathru <user@mathru.net>
2021-01-08 22:56:32 +0900

    バージョンアップ

--- example/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:masamune_core/masamune_core.dart';
-
-void main() async {
-  await Config.init();
-  DataField("name", "mathru");
-  String name = PathMap.get<String>("name");
-  print(name);
-}
